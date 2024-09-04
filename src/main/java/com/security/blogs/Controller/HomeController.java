24beb787f4c294e198ff9c4335a5972e0167519d
adi_dev_97 <93173682+AdiDEV97@users.noreply.github.com>
2024-09-04 20:27:39 +0530

    Update HomeController.java

--- src/main/java/com/security/blogs/Controller/HomeController.java
+++ src/main/java/com/security/blogs/Controller/HomeController.java
@@ -3,6 +3,8 @@ package com.security.blogs.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// Home Controlller
+
 @RestController
 public class HomeController {
 
