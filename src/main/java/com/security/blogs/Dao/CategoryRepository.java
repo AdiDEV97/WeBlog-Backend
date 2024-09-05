d99a549b6d13b3099466c653231d070e0e880cf0
adi_dev_97 <93173682+AdiDEV97@users.noreply.github.com>
2024-09-05 13:09:43 +0530

    Update CategoryRepository.java

--- src/main/java/com/security/blogs/Dao/CategoryRepository.java
+++ src/main/java/com/security/blogs/Dao/CategoryRepository.java
@@ -3,5 +3,7 @@ package com.security.blogs.Dao;
 import com.security.blogs.Model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// Category Repository
+
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 }
