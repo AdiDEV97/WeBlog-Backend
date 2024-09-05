4581ead7cb25f392d95cef811f5e2de919a50a33
adi_dev_97 <93173682+AdiDEV97@users.noreply.github.com>
2024-09-05 13:09:08 +0530

    Update RoleController.java

--- src/main/java/com/security/blogs/Controller/RoleController.java
+++ src/main/java/com/security/blogs/Controller/RoleController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+// Role Controller
+
 @RestController
 @RequestMapping("/roles")
 public class RoleController {
