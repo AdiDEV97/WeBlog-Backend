617a8918960f087d25bc75f7595a8a88774eee37
adi_dev_97 <93173682+AdiDEV97@users.noreply.github.com>
2024-09-06 20:06:20 +0530

    Update UserRepo.java

--- src/main/java/com/security/blogs/Dao/UserRepo.java
+++ src/main/java/com/security/blogs/Dao/UserRepo.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+// User Reposirtory
+
 public interface UserRepo extends JpaRepository<User, Integer> {
 
     Optional<User> findByName(String name);
