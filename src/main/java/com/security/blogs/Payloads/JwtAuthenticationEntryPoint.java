2bb2f0b49746200730141467e4a5c08514aef9c0
adi_dev_97 <93173682+AdiDEV97@users.noreply.github.com>
2024-09-10 12:59:55 +0530

    Update JwtAuthenticationEntryPoint.java

--- src/main/java/com/security/blogs/Payloads/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/security/blogs/Payloads/JwtAuthenticationEntryPoint.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-// Jwt Authentication Entry Point
+// Jwt Authentication Entry Point Class
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
