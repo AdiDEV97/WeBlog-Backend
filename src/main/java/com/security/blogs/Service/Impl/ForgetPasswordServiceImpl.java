a033001fc9e5b572f712bb7cd71c4c3bf21a02a1
adi_dev_97 <93173682+AdiDEV97@users.noreply.github.com>
2024-09-15 11:10:02 +0530

    Update ForgetPasswordServiceImpl.java

--- src/main/java/com/security/blogs/Service/Impl/ForgetPasswordServiceImpl.java
+++ src/main/java/com/security/blogs/Service/Impl/ForgetPasswordServiceImpl.java
@@ -15,6 +15,8 @@ import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 
+// Forget Password Service Implementation
+
 @Service
 public class ForgetPasswordServiceImpl implements ForgetPassowrdService {
 
