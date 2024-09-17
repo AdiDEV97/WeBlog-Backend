263da548f415facee9c528271af8485c6f2da42e
adi_dev_97 <93173682+AdiDEV97@users.noreply.github.com>
2024-09-17 16:19:00 +0530

    Update JwtUtil.java

--- src/main/java/com/security/blogs/Util/JwtUtil.java
+++ src/main/java/com/security/blogs/Util/JwtUtil.java
@@ -11,6 +11,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
+// JWT Util Class
+
 // This class contains methods for -->
 // - generating token
 // - validate
